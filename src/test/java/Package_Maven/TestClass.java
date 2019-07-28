143ef39c759a18dea380b46682c3836ce3493985
Deepak kumar <deepak.kumar260494@gmail.com>
2019-07-28 21:46:09 +0530

    First git commit for my code

---
+++ src/test/java/Package_Maven/TestClass.java
@@ -0,0 +1,50 @@
+package Package_Maven;
+
+import org.testng.annotations.Test;
+
+public class TestClass {
+	
+@Test
+public void add() {
+	int a =10;
+	int b =20;
+	
+	int sum = a+b;
+	
+	System.out.println(sum);
+}
+
+@Test
+public void Sub() {
+	int a =10;
+	int b =20;
+	
+	int sub = a-b;
+	
+	System.out.println(sub);
+}
+
+
+
+@Test
+public void Sub1() {
+	int a =10;
+	int b =20;
+	
+	int sub = a-b;
+	
+	System.out.println(sub);
+}
+
+
+
+@Test
+public void Sub2() {
+	int a =10;
+	int b =20;
+	
+	int sub = a-b;
+	System.out.println(sub);
+}
+
+}
